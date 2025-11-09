f3da239fce755aac1a7c95894651b8a37db97522
AbdurRob <abdurrobr34l@gmail.com>
2025-11-09 22:20:17 +0600

    Installed and connected longodb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 dotenv.config();
 
@@ -11,8 +12,34 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+//* MONOGODB URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@abdurrobr34l.wemtyvn.mongodb.net/?appName=Abdurrobr34l`;
 
-//* ROUTS
+//* MONGODB CLIENT
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+//* MONGODB
+async function run() {
+  try {
+    //? Connect the client to the server
+    await client.connect();
+    //? Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("FinEase is successfully connected to MongoDB!");
+  }
+  finally {
+
+  }
+}
+run().catch(console.dir)
+
+//* -----------ROUTES-----------
 app.get("/", (req, res) => {
   res.send("FinEase Server is running.....");
 });
