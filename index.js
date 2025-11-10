5b85e574db83ccbf438ea8d0c1d9c38d28625802
AbdurRob <abdurrobr34l@gmail.com>
2025-11-10 23:36:57 +0600

    Completed delete route

--- index.js
+++ index.js
@@ -32,66 +32,77 @@ async function run() {
     //? Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("FinEase is successfully connected to MongoDB!");
+
+    const db = client.db("fin_ease");
+    const collection = db.collection("transactions");
+
+    //* -----------ROUTES-----------
+
+    //* -----------ADD TRANSACTION (POST)-----------
+    app.post("/add-transaction", async (req, res) => {
+      const transaction = req.body;
+      try {
+        const result = await collection.insertOne(transaction);
+        res.send(result);
+      }
+      catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Failed to add transaction" });
+      }
+    });
+
+    //* -----------GET TRANSACTION DATA BY ID FROM DB (GET)-----------
+    app.get("/my-transactions/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const result = await collection.findOne({ _id: new ObjectId(id) });
+
+        if (!result) {
+          console.error("Transaction not found");
+          return res.status(500).send({ message: "Failed to fetch transaction data by id" });
+        }
+
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Failed to fetch transaction data by id" });
+      }
+    });
+
+    //* -----------GET ALL TRANSACTION FROM DB (GET)-----------
+    app.get("/my-transactions", async (req, res) => {
+      try {
+        const transactions = await collection.find().toArray();
+        res.send(transactions);
+      }
+      catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Failed to fetch transactions" });
+      }
+    });
+
+    //* -----------DELETE TRANSACTION (DELETE)-----------
+    app.delete("/delete-transaction/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const result = await collection.deleteOne({ _id: new ObjectId(id) });
+        if (result.deletedCount === 0) {
+          return res.status(404).send({ message: "Transaction not found" });
+        }
+        res.send({ message: "Transaction deleted successfully" });
+      }
+      catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Failed to delete transaction" });
+      }
+    })
+
   }
   finally {
 
   }
 }
 run().catch(console.dir)
-
-//* -----------ROUTES-----------
-app.get("/", (req, res) => {
-  res.send("FinEase Server is running.....");
-});
-
-//* -----------ADD TRANSACTION (POST)-----------
-app.post("/add-transaction", async (req, res) => {
-  const transaction = req.body;
-
-  try {
-    const db = client.db("fin_ease");
-    const collection = db.collection("transactions");
-    const result = await collection.insertOne(transaction);
-      res.send(result);
-  }
-  catch (error) {
-    console.error(error);
-    res.status(500).send({ message: "Failed to add transaction" });
-  }
-})
-
-//* -----------GET TRANSACTION DATA BY ID FROM DB (GET)-----------
-app.get("/my-transactions/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const db = client.db("fin_ease");
-    const collection = db.collection("transactions");
-    const result = await collection.findOne({_id: new ObjectId(id)});
-
-    if(!result) {
-      console.error(error);
-    res.status(500).send({ message: "Failed to fetch transaction data by id" });
-  }
-  res.send(result);
-    
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({ message: "Failed to fetch transaction data by id" });
-  }
-})
-
-//* -----------GET ALL TRANSACTION FROM DB (GET)-----------
-app.get("/my-transactions", async (req, res) => {
-  try {
-    const db = client.db("fin_ease");
-    const collection = db.collection("transactions")
-    const transactions = await collection.find().toArray()
-    res.send(transactions)
-  }
-  catch (error) {
-    res.status(500).send({ message: "Failed to fetch transactions" });
-  }
-})
 
 //* START SERVER
 app.listen(port, () => {
