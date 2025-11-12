288871fae822db1c7d36074815bba0ba4aea7f45
AbdurRob <abdurrobr34l@gmail.com>
2025-11-12 23:23:58 +0600

    Updated codes to solve errors

--- index.js
+++ index.js
@@ -28,20 +28,24 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     //? Connect the client to the server
-    await client.connect();
+    // await client.connect();
     //? Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("FinEase is successfully connected to MongoDB!");
 
     const db = client.db("fin_ease");
     const collection = db.collection("transactions");
 
     //* -----------ROUTES-----------
+    app.get("/", (req, res) => {
+      res.send("Server running")
+    })
 
     //* -----------ADD TRANSACTION (POST)-----------
     app.post("/add-transaction", async (req, res) => {
       const transaction = {
         ...req.body,
+        userEmail: req.body.userEmail,
         createdAt: new Date(),
       };
       try {
@@ -69,12 +73,18 @@ async function run() {
     //* -----------GET ALL TRANSACTION FROM DB (GET)-----------
     app.get("/my-transactions", async (req, res) => {
       try {
-        const sortBy = req.query.sortBy || "createdAt";   // default sort field
-    const order = req.query.order === "asc" ? 1 : -1; // default descending
-    const transactions = await collection.find().sort({ [sortBy]: order }).toArray();
+        const { email, sortBy = "createdAt", order = "desc" } = req.query;
+        if (!email) return res.status(400).send({ message: "User email is required" });
+
+        const sortOrder = order === "asc" ? 1 : -1;
+
+        const transactions = await collection
+          .find({ userEmail: { $regex: `^${email.trim()}$`, $options: "i" } }) // case-insensitive
+          .sort({ [sortBy]: sortOrder })
+          .toArray();
+
         res.send(transactions);
-      }
-      catch (error) {
+      } catch (error) {
         console.error(error);
         res.status(500).send({ message: "Failed to fetch transactions" });
       }
